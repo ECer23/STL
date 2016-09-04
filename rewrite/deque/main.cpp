88c8082edf9a2bdf0c3a72fdf363531d86db7ff5
Ecr-nix <ecr23pg@gmail.com>
2016-09-04 11:23:16 +0800

    update ccomment

--- rewrite/deque/main.cpp
+++ rewrite/deque/main.cpp
@@ -1,32 +1,36 @@
-#include "Deque.h"
+#include <exception>
 #include <iostream>
-#include<exception>
- using namespace std;
- class RuleForbidden: public exception {
+#include "Deque.h"
+using namespace std;
+class RuleForbidden : public exception {
   virtual const char *what() const throw() {
     return "Please accept the rules in this assignment.";
   }
 };
- int main() {
-  // rule check.
-  #if (defined(_GLIBCXX_LIST) || defined(_GLIBCXX_VECTOR) ||\
-    defined(_GLIBCXX_DEQUE) || defined(_GLIBCXX_STACK))
-    throw RuleForbidden();
-  #endif
+int main() {
+// rule check.
+#if (defined(_GLIBCXX_LIST) || defined(_GLIBCXX_VECTOR) || \
+     defined(_GLIBCXX_DEQUE) || defined(_GLIBCXX_STACK))
+  throw RuleForbidden();
+#endif
   if (MAP_SIZE != 20 || BUFFER_SIZE != 10) throw RuleForbidden();
-   deque<int> que2;
-   if (que2.empty()) cout << "The que is empty!" << endl;
-  else cout << "The queue is not empty!" << endl;
-   // test for iterator.
+  deque<int> que2;
+  if (que2.empty())
+    cout << "The que is empty!" << endl;
+  else
+    cout << "The queue is not empty!" << endl;
+  // test for iterator.
   int m, n;
   cin >> m >> n;
-   for (int i = 0; i < m; i++) que2.push_front(i);
+  for (int i = 0; i < m; i++) que2.push_front(i);
   deque<int>::iterator it = que2.begin();
   while (it != que2.end()) cout << *(it++) << " ";
   cout << endl;
   cout << "The size of deque is: " << que2.size() << endl;
-  if (que2.empty()) cout << "The que is empty!" << endl;
-  else cout << "The queue is not empty!" << endl;
+  if (que2.empty())
+    cout << "The que is empty!" << endl;
+  else
+    cout << "The queue is not empty!" << endl;
   que2.pop_front();
   que2.pop_back();
   cout << "The front is: " << que2.front() << endl;
@@ -34,8 +38,8 @@
   cout << que2[0] << endl;
   cout << que2[que2.size() - 1] << endl;
   cout << que2[n] << endl;
-   cin >> m >> n;
-   for (int i = 0; i < m; i++) que2.push_back(i);
+  cin >> m >> n;
+  for (int i = 0; i < m; i++) que2.push_back(i);
   it = que2.begin();
   while (it != que2.end()) cout << *(it++) << " ";
   cout << endl;
@@ -45,14 +49,14 @@
   cout << que2[0] << endl;
   cout << que2[que2.size() - 1] << endl;
   cout << que2[n] << endl;
-   que2.pop_front();
+  que2.pop_front();
   que2.pop_front();
   que2.pop_back();
-   cout << "Now the queue is: " << endl;
+  cout << "Now the queue is: " << endl;
   it = que2.begin();
   while (it != que2.end()) cout << *(it++) << " ";
   cout << endl;
-   deque<int>::iterator tmp1 = que2.begin();
+  deque<int>::iterator tmp1 = que2.begin();
   deque<int>::iterator tmp2 = tmp1, tmp3;
   tmp2 = tmp2 + 5;
   cout << *(tmp2++) << endl;
@@ -72,8 +76,8 @@
   cout << tmp1[0] << endl;
   cout << tmp1[8] << endl;
   cout << tmp1[23] << endl;
-  cout << tmp2[-3] << endl;
-   que2.clear();
+  cout << "*" << tmp2[3] << endl;
+  que2.clear();
   cout << "The size of deque is: " << que2.size() << endl;
   m = 13;  // random input m > 20 and m < 30..
   for (int i = 0; i < m; i++) que2.push_front(i);
--- rewrite/strVec/strVec.cpp
+++ rewrite/strVec/strVec.cpp
@@ -1,12 +1,11 @@
 #include "strVec.h"
+#include <utility>  // std::move
 
-strVector::strVector() { 
-  elements = first_free = cap = nullptr;
-}
+strVector::strVector() { elements = first_free = cap = nullptr; }
 
 strVector::~strVector() { free(); }
 
-void strVector::push_back(const string &s) {
+void strVector::push_back(const string& s) {
   check_alloc();
   alloc.construct(first_free++, s);
 }
@@ -21,35 +20,41 @@ void strVector::pop_back() {
 
 size_t strVector::size() const { return size_t(first_free - elements); }
 size_t strVector::capacity() const { return size_t(cap - elements); }
-string *strVector::begin() const { return elements; }
-string *strVector::end() const { return first_free; }
+string* strVector::begin() const { return elements; }
+string* strVector::end() const { return first_free; }
 
 void strVector::check_alloc() {
-  if (this->size() == this->capacity())
-    reallocate();
+  if (this->size() == this->capacity()) reallocate();
+}
+
+void strVector::reallocate() {
+  auto newcapacity = size() ? 2 * size() : 1;
+  alloc_n_move(newcapacity);
 }
 
 void strVector::alloc_n_move(size_t new_cap) {
   auto newdata = alloc.allocate(new_cap);
-  auto dest = newdata;
   auto elem = elements;
-  for (size_t i = 0; i != size(); ++i)
+  auto dest = newdata;
+  // use std::move() rather than move()
+  for (auto i = 0; i != size(); i++) {
     alloc.construct(dest++, std::move(*elem++));
+  }
   free();
-  elements = newdata;
   first_free = dest;
+  elements = newdata;
   cap = elements + new_cap;
-}
-
-void strVector::reallocate() {
-  auto newcapcity = size() ? 2 * size() : 1;
-  alloc_n_move(newcapcity);
 }
 
 void strVector::free() {
   if (elements != nullptr) {
-    for (auto p = first_free; p != elements;)
+    // first_free is pointing to the first unconstructed string block
+    // [ 1 ][ 2 ][  ][  ]
+    //        ^    ^
+    //       --p   p
+    for (auto p = first_free; p != elements; /* empty */) {
       alloc.destroy(--p);
+    }
     alloc.deallocate(elements, capacity());
   }
 }
